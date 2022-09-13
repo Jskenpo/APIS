914e8b4fdd906a72927af0c5a98b0ec551d743a6
jsantis <san21153@uvg.edu.gt>
2022-09-13 00:05:11 -0600

    final changes

--- LABS_APIS/app/src/main/java/com/plataformas/labs_apis/Character_Details_Fragment.kt
+++ LABS_APIS/app/src/main/java/com/plataformas/labs_apis/Character_Details_Fragment.kt
@@ -1,11 +1,55 @@
 package com.plataformas.labs_apis
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.fragment.navArgs
+import coil.load
+import coil.request.CachePolicy
+import coil.transform.CircleCropTransformation
 
-class Character_Details_Fragment : Fragment() {
+class Character_Details_Fragment : Fragment(R.layout.fragment_character__details_) {
 
+
+    lateinit var imagen : ImageView
+    lateinit var nombre : TextView
+    lateinit var estado : TextView
+    lateinit var especie : TextView
+    lateinit var genero : TextView
+    private val args: Character_Details_FragmentArgs by navArgs()
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        imagen = view.findViewById(R.id.image_details)
+        nombre = view.findViewById(R.id.text_details_name)
+        estado = view.findViewById(R.id.text_details_status)
+        especie = view.findViewById(R.id.text_details_species)
+        genero = view.findViewById(R.id.text_details_gender)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun getCharacterDetails() {
+        val character = args.character
+        getImage(args.character.image)
+        nombre.text = "Nombre: "+character.name
+        estado.text = "Estado: "+character.status
+        especie.text = "Especie: "+character.species
+        genero.text = "Género: "+character.gender
+    }
+
+    private fun getImage(direc:String) {
+        imagen.load(direc){
+            transformations(CircleCropTransformation())
+            error(R.drawable.ic_baseline_error_outline_24)
+            diskCachePolicy(CachePolicy.ENABLED)
+            memoryCachePolicy(CachePolicy.ENABLED)
+        }
+
+    }
 }
--- LABS_APIS/app/src/main/java/com/plataformas/labs_apis/FragmentList.kt
+++ LABS_APIS/app/src/main/java/com/plataformas/labs_apis/FragmentList.kt
@@ -5,22 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.plataformas.labs_apis.adapter.Adapter
 import com.plataformas.labs_apis.database.Character
+import com.plataformas.labs_apis.database.RickAndMortyDB
 
 
-class FragmentList : Fragment(R.layout.fragment_list) {
+class FragmentList() : Fragment(R.layout.fragment_list), Adapter.RecyclerViewCharacterClickHandler {
 
-lateinit var recycler: RecyclerView
-lateinit var list: MutableList<Character>
-lateinit var adapter: Adapter
+    private lateinit var recycler: RecyclerView
+    private lateinit var list: MutableList<Character>
+    private lateinit var adapter: Adapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recycler = view.findViewById(R.id.listofCharacters)
-
+        initRecycler()
 
     }
+
+    private fun initRecycler() {
+        list= RickAndMortyDB.getCharacters()
+        adapter = Adapter(list, this)
+
+        recycler.layoutManager = LinearLayoutManager(requireContext())
+        recycler.adapter = adapter
+    }
+
+    override fun onCharacterClicked(character: Character) {
+        val action = FragmentListDirections.actionFragmentListToCharacterDetailsFragment(character)
+        requireView().findNavController().navigate(action)
+    }
+
 
 }
