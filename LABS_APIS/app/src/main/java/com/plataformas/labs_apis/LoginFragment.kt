f7bb2d7a4b4a5002cf4e04e7de5831e9788fd98e
jsantis <san21153@uvg.edu.gt>
2022-10-04 09:43:43 -0600

    login fragment terminado

--- LABS_APIS/app/src/main/java/com/plataformas/labs_apis/LoginFragment.kt
+++ LABS_APIS/app/src/main/java/com/plataformas/labs_apis/LoginFragment.kt
@@ -6,7 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.findNavController
 import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
@@ -19,9 +25,51 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         Email = view.findViewById(R.id.textInputLayoutEmail)
         Password = view.findViewById(R.id.textInputLayoutPassword)
         buttonLog = view.findViewById(R.id.buttonLogin)
+
+        verificar()
+        setListeners()
     }
 
-    
+    private fun verificar(){
+        CoroutineScope(Dispatchers.IO).launch{
+            val actual = requireContext().dataStore.getPreferencesValue(KEY_EMAIL)
+            if (actual != null){
+                navegar()
+            }
+
+        }
+    }
+
+    private fun setListeners(){
+        buttonLog.setOnClickListener {
+            login(
+                email = Email.editText!!.text.toString(),
+                password = Password.editText!!.text.toString()
+            )
+        }
+    }
+
+    private fun login(email:String, password:String){
+        if ((email == getString(R.string.correo)) && (email == password )){
+            guardar(email)
+        }else{
+            Toast.makeText(requireContext(), "Usuario o contraseña incorrectos", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun navegar(){
+        CoroutineScope(Dispatchers.Main).launch{
+            requireView().findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToFragmentList())
+        }
+    }
+
+    private fun guardar(email:String){
+        CoroutineScope(Dispatchers.IO).launch {
+            requireContext().dataStore.savePreferencesValue(KEY_EMAIL, email)
+            navegar()
+        }
+    }
+
 
 
 }
